1abb7533f4dd6c7cb9bc7c769e1853efecd3f2ec
xxx <616896861@qq.com>
2026-05-13 01:10:36 -0700

    refactor(cmd): move TestMain to dedicated file

--- cmd/openbee/config_claude_test.go
+++ cmd/openbee/config_claude_test.go
@@ -1,16 +1,1 @@
 package main
-
-import (
-	"os"
-	"testing"
-
-	"github.com/theopenbee/openbee/internal/infra/i18n"
-)
-
-func TestMain(m *testing.M) {
-	// Load default locale so that i18n.M fields are populated for all tests.
-	if err := i18n.Load("en"); err != nil {
-		panic("i18n.Load failed: " + err.Error())
-	}
-	os.Exit(m.Run())
-}
---
+++ cmd/openbee/testmain_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/theopenbee/openbee/internal/infra/i18n"
+)
+
+func TestMain(m *testing.M) {
+	if err := i18n.Load("en"); err != nil {
+		panic("i18n.Load failed: " + err.Error())
+	}
+	os.Exit(m.Run())
+}
